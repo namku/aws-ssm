05161ee9379b86970247e64e6927412885c04f75
namku <syak7771@gmail.com>
2022-05-26 09:52:13 +0200

    Update version to v0.1.4

--- cmd/root.go
+++ cmd/root.go
@@ -26,7 +26,7 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:     "aws-ssm",
-	Version: "v0.1.3",
+	Version: "v0.1.4",
 	Short:   "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
